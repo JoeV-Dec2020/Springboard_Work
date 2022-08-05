fce8baf29d784073db2d80298bf6399825b9d9f2
Joe Virnig <75699136+JoeV-Dec2020@users.noreply.github.com>
2022-08-04 20:57:48 -0500

    Delete autoinc_mapper1.py

--- autoinc_mapper1.py
+++
@@ -1,8 +0,0 @@
-# !/usr/bin/env python
-import sys
-
-# input comes from STDIN (standard input)
-for line in sys.stdin:
-    # [derive mapper output key values]
-    rec_parts = line.split(',')
-    print '%s' % (rec_parts[2] + ',' + rec_parts[1] + ',' + rec_parts[3] + ',' + rec_parts[4] + ',' + rec_parts[5])
