13a9380449c323bbab0701eeeaa410900ddb9641
Joe Virnig <75699136+JoeV-Dec2020@users.noreply.github.com>
2022-08-04 20:34:20 -0500

    Delete sql_q18_sol.sql

--- sql_q18_sol.sql
+++
@@ -1,8 +0,0 @@
--- find the highest number of foul cards given in one match
-select max(match_fouls) max_match_fouls
- from  (select count(*) match_fouls
-         from  player_booked
-         group by match_no) tmp_tbl;
-
--- max_match_fouls
--- '10'
