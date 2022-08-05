da84e473de9a1c6d64f417125864a3dce1640b5b
Joe Virnig <75699136+JoeV-Dec2020@users.noreply.github.com>
2022-08-04 20:57:26 -0500

    Delete Accident_Data_Stream.sh

--- Accident_Data_Stream.sh
+++
@@ -1,9 +0,0 @@
-hadoop jar /usr/local/hadoop/contrib/streaming/hadoop-*streaming*.jar \
--file autoinc_mapper1.py -mapper autoinc_mapper1.py \
--file autoinc_reducer1.py -reducer autoinc_reducer1.py \
--input input/data.csv -output output/all_accidents
-
-hadoop jar /usr/local/hadoop/contrib/streaming/hadoop-*streaming*.jar \
--file autoinc_mapper2.py -mapper autoinc_mapper2.py \
--file autoinc_reducer2.py -reducer autoinc_reducer2.py \
--input output/all_accidents -output output/make_year_count
